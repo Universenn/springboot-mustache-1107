0d400b3d00a7eb63100586fc1f32d9e5ab3f2670
Universenn <dnwndhks98@gmail.com>
2022-11-07 11:15:35 +0900

    Springboot Mustache Bbs 빌드 1. application.yml

---
+++ src/main/java/com/mustache/bbs/SpringbootMustache1107Application.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustache1107Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMustache1107Application.class, args);
+    }
+
+}
